85d4996a17b348c6376a4864dfa5d961fab90d97
HunterXVD11 <81260156+HunterXVD11@users.noreply.github.com>
2021-11-22 14:34:33 -0300

    Criando a primeira versão da primeira tarefa

---
+++ AtividadeBacktracking.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+void imprimirCombinacoes(char* vet, int pos, int n);
+
+int main(){
+    char vet[3] = {'a','e','i'};
+    imprimirCombinacoes(vet,0,3);
+}
+
+void imprimirCombinacoes(char* vet, int pos, int n){
+    int i;
+    if (pos == n){
+        int contadorA = 0;
+        int contadorE = 0;
+        int contadorI = 0;
+        for (i = 0; i < n; i++) {
+            if(vet[i] == 'a'){contadorA++;}
+            else if (vet[i] == 'e'){contadorE++;}
+            else {contadorI++;}
+        }
+        if(contadorA > (contadorI+contadorE)){
+            for (int j = 0; j < n; ++j) {
+                printf("%c",vet[i]);
+                printf("\n");
+            }
+        }
+    }
+    else{
+        vet[pos] = 'a';
+        imprimirCombinacoes(vet,pos+1,n);
+        vet[pos] = 'e';
+        imprimirCombinacoes(vet,pos+1,n);
+        vet[pos] = 'i';
+        imprimirCombinacoes(vet,pos+1,n);
+    }
+}
---
+++ cmake-build-debug/CMakeFiles/Backtracking.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "Backtracking.exe"
+  "Backtracking.exe.manifest"
+  "Backtracking.pdb"
+  "CMakeFiles/Backtracking.dir/AtividadeBacktracking.c.obj"
+  "libBacktracking.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Backtracking.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
